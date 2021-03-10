21686830582e5c9909683adc22428c23b95dfc02
lambert.wang <lambert.wang@crediteyes.com>
2021-03-10 14:37:28 +0800

    多余代码删除

--- finder-web/src/main/java/com/skin/finder/servlet/ReceiveServlet.java
+++ finder-web/src/main/java/com/skin/finder/servlet/ReceiveServlet.java
@@ -16,7 +16,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.oracle.xmlns.internal.webservices.jaxws_databinding.JavaWsdlMappingType;
 import com.skin.finder.acl.SimpleUserManager;
 import com.skin.finder.acl.User;
 import com.skin.finder.acl.UserManager;
